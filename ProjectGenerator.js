3f1ee365ba00af9f860df5a7dac305e361d85389
jayeshpaluru <113400123+jayeshpaluru@users.noreply.github.com>
2023-01-19 13:12:16 -0600

    Fixed a bug

--- ProjectGenerator.js
+++ ProjectGenerator.js
@@ -1,83 +1,74 @@
 const API_KEY = "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";
-      const languageSelect = document.getElementById("languageSelect");
-      const skillSelect = document.getElementById("skillSelect");
-      const submitBtn = document.getElementById("submitBtn");
-      const displayProjectIdea = document.getElementById("displayProjectIdea");
-      let cache = {};
+const { value: languageSelect } = document.getElementById("languageSelect");
+const { value: skillSelect } = document.getElementById("skillSelect");
+const submitBtn = document.getElementById("submitBtn");
+const displayProjectIdea = document.getElementById("displayProjectIdea");
+let cache = {};
 
-      async function getProjectIdea(language, skill) {
-        document.getElementById("loading").style.display = "block"; // show loading animation
-        const userInput = `Generate a project idea for a ${skill} level ${language} developer.`;
-        if (cache[userInput] && cache[userInput].timestamp > Date.now() - 86400000) {
-          console.log("Response retrieved from cache");
-          document.getElementById("loading").style.display = "none"; // hide loading animation
-          return cache[userInput].data;
-        }
+async function getProjectIdea(language, skill) {
+  document.getElementById("loading").style.display = "block"; 
+  const userInput = `Generate a project idea for a ${skill} level ${language} developer.`;
+  if (cache[userInput] && cache[userInput].timestamp > Date.now() - 86400000) {
+    console.log("Response retrieved from cache");
+    document.getElementById("loading").style.display = "none";
+    return cache[userInput].data;
+  }
+  console.log("getProjectIdea function called");
+  const endpoint = "https://api.openai.com/v1/engines/text-davinci-002/completions";
+  const body = {
+    prompt: userInput,
+    max_tokens: 250,
+    stop: "Project completed",
+    temperature: 0.75
+  }
+  try {
+    const response = await fetch(endpoint, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${API_KEY}`
+      },
+      body: JSON.stringify(body)
+    });
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+    const { choices } = await response.json();
+    if (choices && choices[0] && choices[0].text) {
+      const { text } = choices[0];
+      cache[userInput] = { data: text, timestamp: Date.now() };
+      document.getElementById("loading").style.display = "none"; 
+      return text;
+    } else {
+      throw new Error("No text returned from API");
+    }
+  } catch (err) {
+    console.error(err);
+    document.getElementById("loading").style.display = "none"; 
+  }
+}
 
-        console.log("getProjectIdea function called");
-        const endpoint = "https://api.openai.com/v1/engines/text-davinci-002/completions";
-
-        const body = {
-          prompt: userInput,
-          max_tokens: 250,
-          stop: "Project completed",
-          temperature: 0.75
-        }
-
-        try {
-          const response = await fetch(endpoint, {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-              "Authorization": `Bearer ${API_KEY}`
-            },
-            body: JSON.stringify(body)
-        });
-          const json = await response.json();
-          if (json.choices && json.choices[0].text) {
-            cache[userInput] = { data: json.choices[0].text, timestamp: Date.now() };
-            document.getElementById("loading").style.display = "none"; // hide loading animation
-            return json.choices[0].text;
-          } else {
-            throw new Error("No text returned from API");
-          }
-        } catch (err) {
-          console.error(err);
-          document.getElementById("loading").style.display = "none"; // hide loading animation
-        }
+submitBtn.addEventListener("click", async function() {
+  const selectedLanguage = languageSelect;
+  const selectedSkill = skillSelect;
+  try {
+    const projectIdeas = await getProjectIdea(selectedLanguage, selectedSkill);
+    console.log(projectIdeas);
+    displayProjectIdea.textContent = "";
+    const list = document.createElement("ul");
+    list.style.listStyleType = "none";
+    list.style.paddingLeft = "20px";
+    projectIdeas.split("\n").forEach(idea => {
+      if (idea !== "") {
+        const item = document.createElement("li");
+        item.textContent = idea;
+        list.appendChild(item);
       }
-      submitBtn.addEventListener("click", function() {
-        const selectedLanguage = languageSelect.value;
-        const selectedSkill = skillSelect.value;
-        getProjectIdea(selectedLanguage, selectedSkill).then(projectIdea => {
-          displayProjectIdea.textContent = projectIdea;
-        });
-      });
+    });
+    displayProjectIdea.appendChild(list);
+  } catch (error) {
+    console.log(error);
+    displayProjectIdea.textContent = "Error: " + error.message;
+  }
+});
 
-      async function main() {
-        submitBtn.addEventListener("click", async function() {
-          const language = languageSelect.value;
-          const skill = skillSelect.value;
-          try {
-            const projectIdeas = await getProjectIdea(language, skill);
-            console.log(projectIdeas);
-            displayProjectIdea.innerHTML = ""; // Clear the previous content
-            const list = document.createElement("ul"); // Create a new unordered list element
-            list.style.listStyleType = "none"; // Remove bullet points
-            list.style.paddingLeft = "20px"; // Indent the bullet points
-            projectIdeas.split("\n").forEach(idea => { // Split the text into lines
-              if (idea !== "") { // Ignore empty lines
-                const item = document.createElement("li"); // Create a new list item
-                item.innerText = idea; // Set the text of the list item
-                list.appendChild(item); // Add the list item to the unordered list
-              }
-            });
-            displayProjectIdea.appendChild(list); // Add the unordered list to the displayProjectIdea element
-          } catch (error) {
-            console.log(error);
-            displayProjectIdea.innerText = "Error: " + error.message;
-          }
-        });
-      }
-
-      main();
