4eceb3f5edf5c39834c1abbcc124995fdb7d7df6
jayeshpaluru <113400123+jayeshpaluru@users.noreply.github.com>
2023-01-18 20:57:27 -0600

    Update index.js
    Made the code functional with a website

--- index.js
+++ index.js
@@ -1,42 +1,75 @@
-const fetch = require("isomorphic-fetch");
+const API_KEY = "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";
+      const languageSelect = document.getElementById("languageSelect");
+      const skillSelect = document.getElementById("skillSelect");
+      const submitBtn = document.getElementById("submitBtn");
+      const displayProjectIdea = document.getElementById("displayProjectIdea");
+      let cache = {};
 
-const API_KEY = "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"
+      async function getProjectIdea(language, skill) {
+        const userInput = `Generate a project idea for a ${skill} level ${language} developer.`;
+        if (cache[userInput] && cache[userInput].timestamp > Date.now() - 86400000) {
+          console.log("Response retrieved from cache");
+          return cache[userInput].data;
+        }
+        
+        console.log("getProjectIdea function called");
+        const endpoint = "https://api.openai.com/v1/engines/text-davinci-002/completions";
 
-async function getProjectIdea(userInput) {
-    const endpoint = "https://api.openai.com/v1/engines/text-davinci-002/completions"
+        const body = {
+          prompt: userInput,
+          max_tokens: 250,
+          stop: "Project completed",
+          temperature: 0.75
+        }
 
-    const body = {
-        prompt: userInput,
-        max_tokens: 250,
-        stop: "Project completed",
-        temperature: 0.75
-    }
-
-    try {
-        const response = await fetch(endpoint, {
+        try {
+          const response = await fetch(endpoint, {
             method: "POST",
             headers: {
-                "Content-Type": "application/json",
-                "Authorization": `Bearer ${API_KEY}`
+              "Content-Type": "application/json",
+              "Authorization": `Bearer ${API_KEY}`
             },
             body: JSON.stringify(body)
         });
-        const json = await response.json();
-        if (json.choices && json.choices.length > 0) {
+          const json = await response.json();
+          if (json.choices && json.choices.length > 0) {
+            cache[userInput] = {
+              data: json.choices[0].text,
+              timestamp: Date.now()
+            };
             return json.choices[0].text;
-        } else {
-            console.error('No project ideas found');
-            return '';
+          } else {
+            throw new Error('No project ideas found');
+          }
+        } catch (error) {
+          console.log(error);
         }
-    } catch (err) {
-        console.error(err);
-    }
-}
+      }
 
-async function main() {
-    const userInput = "I am a beginner coder and I want to create a project using java, what are some project ideas that I can work on?";
-    const projectIdea = await getProjectIdea(userInput);
-    console.log(projectIdea);
-}
+      async function main() {
+        submitBtn.addEventListener("click", async function() {
+          const language = languageSelect.value;
+          const skill = skillSelect.value;
+          try {
+            const projectIdeas = await getProjectIdea(language, skill);
+            console.log(projectIdeas);
+            displayProjectIdea.innerHTML = ""; // Clear the previous content
+            const list = document.createElement("ul"); // Create a new unordered list element
+            list.style.listStyleType = "none"; // Remove bullet points
+            list.style.paddingLeft = "20px"; // Indent the bullet points
+            projectIdeas.split("\n").forEach(idea => { // Split the text into lines
+              if (idea !== "") { // Ignore empty lines
+                const item = document.createElement("li"); // Create a new list item
+                item.innerText = idea; // Set the text of the list item
+                list.appendChild(item); // Add the list item to the unordered list
+              }
+            });
+            displayProjectIdea.appendChild(list); // Add the unordered list to the displayProjectIdea element
+          } catch (error) {
+            console.log(error);
+            displayProjectIdea.innerText = "Error: " + error.message;
+          }
+        });
+      }
 
-main();
+      main();
